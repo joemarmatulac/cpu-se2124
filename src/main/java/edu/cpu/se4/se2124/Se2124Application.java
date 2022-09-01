bd6602e68917ad8844e414fca4480de0ed5c24c5
joemarmatulac <joemar.matulac@gmail.com>
2022-09-01 14:33:09 +0800

    Initial commit.

---
+++ src/main/java/edu/cpu/se4/se2124/Se2124Application.java
@@ -0,0 +1,14 @@
+package edu.cpu.se4.se2124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2124Application.class, args);
+		System.out.println("Hello SE2124");
+	}
+
+}
---
+++ src/test/java/edu/cpu/se4/se2124/Se2124ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpu.se4.se2124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
